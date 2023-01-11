1f47a78e53e6ac31acaa3c225a20970cd0c6e622
abemaru <ch4nge.lightening@gmail.com>
2023-01-11 23:48:45 +0900

    add strcut

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"github.com/gocolly/colly"
 )
 
+type DownloadImage struct {
+	Name		string
+	Image 	string
+}
+
 func main() {
 	c := colly.NewCollector()
+	c.SetRequestTimeout(120 * time.Second)
+	// download_image = make([]DownloadImage, 0)
 
 	c.OnHTML("a", func(e *colly.HTMLElement) {
 		e.Request.Visit(e.Attr("href"))
